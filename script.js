ad30cc257cf843b694c5d69d10bde78667432e93
MaayanChr <maayan.chr@gmail.com>
2026-05-02 12:19:32 +0300

    5

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
 const links = [
   {
-    title: "מפה מצטברת",
-    url: "https://bishvilyael.github.io/FullMap/"
+    title: "מפה מצטברת מתחילת המיזם 4089 נקודות",
+    url: "https://bishvilyael.github.io/FullMap_J_Split/"
   },
-  {
-    title: "מפה תקופתית",
+{
+    title: "מפה חדשית - ניתן להחליף חודש בתוך המפה",
     url: "https://bishvilyael.github.io/PartialMap_Multi/"
   },
-  {
+ {
+    title: "מפה מצטברת  Google Maps",
+    url: "https://www.google.com/maps/d/edit?mid=1ZV3wyw_U2qiEHsuJ9LmLKnY8BGG57NU&usp=sharing"
+  },
+{
+    title: "קישור לאתר - בשביל יעל",
+    url: "https://https://www.beshvilyael.com/"
+  }
+  /* {
     title: "מפות אישיות לפי יעל",
     url: "https://bishvilyael.github.io/Badge_Map/"
-  }
+  } */
 ];
 
 const container = document.getElementById("linksContainer");
