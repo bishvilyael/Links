cfbbd8da432981468b0349706f564c31daf56019
MaayanChr <maayan.chr@gmail.com>
2026-05-03 12:21:53 +0300

    VIDEO

--- script.js
+++ script.js
@@ -3,22 +3,18 @@ const links = [
     title: "מפה מצטברת מתחילת המיזם - 4297 נקודות במפה",
     url: "https://bishvilyael.github.io/FullMap_J_Split/"
   },
-{
+  {
     title: "מפה חדשית - ניתן להחליף חודש בתוך המפה",
     url: "https://bishvilyael.github.io/PartialMap_Multi/"
   },
- {
-    title: "מפה מצטברת  Google Maps",
+  {
+    title: "מפה מצטברת Google Maps",
     url: "https://www.google.com/maps/d/edit?mid=1ZV3wyw_U2qiEHsuJ9LmLKnY8BGG57NU&usp=sharing"
   },
-{
+  {
     title: "קישור לאתר - בשביל יעל",
     url: "https://www.beshvilyael.com/"
   }
-  /* {
-    title: "מפות אישיות לפי יעל",
-    url: "https://bishvilyael.github.io/Badge_Map/"
-  } */
 ];
 
 const container = document.getElementById("linksContainer");
@@ -29,10 +25,34 @@ links.forEach(item => {
   link.className = "link-button";
   link.href = item.url;
   link.textContent = item.title;
-
-  // פתיחה בלשונית חדשה
   link.target = "_blank";
   link.rel = "noopener noreferrer";
 
   container.appendChild(link);
+});
+
+/* כפתור לפתיחת פלייליסט מוטמע */
+const playlistButton = document.createElement("button");
+playlistButton.className = "link-button";
+playlistButton.textContent = "סרטוני תמונות חדשיים ועוד";
+container.appendChild(playlistButton);
+
+const videoSection = document.createElement("div");
+videoSection.className = "video-section hidden";
+videoSection.innerHTML = `
+  <div class="video-wrapper">
+    <iframe
+      src="https://www.youtube.com/embed/videoseries?list=PLEfko1eUSu2Uw5Q22xmRMpzxNR5JFroFE"
+      title="YouTube playlist"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen>
+    </iframe>
+  </div>
+`;
+
+container.appendChild(videoSection);
+
+playlistButton.addEventListener("click", () => {
+  videoSection.classList.toggle("hidden");
 });
